c091e4dc376791cae04702aaec4b80ca2a703a34
Juani <juanigramajo@hotmail.com>
2022-05-17 16:03:38 -0300

    ejemplo de conversión y suma

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,7 +7,17 @@ Console.WriteLine("Trying second commit");
 
 
 
-Console.WriteLine("Ingrese un texto: ");
+/* Console.WriteLine("Ingrese un texto: ");
 string texto = Console.ReadLine();
 int numero = 4;
-Console.WriteLine("Usted escribió [" + texto + "]. | Y el número es [" + numero + "]");
+Console.WriteLine("Usted escribió [" + texto + "]. | Y el número es [" + numero + "]"); */
+
+
+
+Console.WriteLine("Ingrese un número: ");
+string numero1 = Console.ReadLine();
+Console.WriteLine("Ingrese otro número: ");
+string numero2 = Console.ReadLine();
+int resultado = Convert.ToInt32(numero1) + Convert.ToInt32(numero2);
+Console.WriteLine("La suma de ambos números es " + resultado);
+
