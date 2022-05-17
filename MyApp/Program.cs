704923b13bc371deb6d529c4edd9cf8f68cae404
Juani <juanigramajo@hotmail.com>
2022-05-17 15:38:50 -0300

    primer ejemplo de texto

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,4 +2,12 @@
 Console.WriteLine("Hello, World!");
 
 
+
 Console.WriteLine("Trying second commit");
+
+
+
+Console.WriteLine("Ingrese un texto: ");
+string texto = Console.ReadLine();
+int numero = 4;
+Console.WriteLine("Usted escribió [" + texto + "]. | Y el número es [" + numero + "]");
