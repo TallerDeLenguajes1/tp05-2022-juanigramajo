efc567c73e6203521aa3d953c9246bed2012be68
Juani <juanigramajo@hotmail.com>
2022-05-24 15:46:16 -0300

    punto 2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -41,7 +41,7 @@ double Cuadrado(double a){
 
 //-------------------EJERCICIO 1-------------------
 
-Console.WriteLine("Ingrese un número: ");
+/* Console.WriteLine("Ingrese un número: ");
 string num = Console.ReadLine();
 
 int numInvertir = Convert.ToInt32(num);
@@ -55,4 +55,84 @@ while (numInvertir > 0){
     
 }
 
-Console.WriteLine(numInvertido);
+Console.WriteLine(numInvertido); */
+
+
+
+
+//-------------------EJERCICIO 2-------------------
+
+
+int dato = menu();
+calculos(dato);
+
+
+void calculos(int dato){
+
+    Console.WriteLine("Ingrese un número: ");
+    string num1 = Console.ReadLine();
+    Console.WriteLine("Ingrese otro número: ");
+    string num2 = Console.ReadLine();
+
+    int valor;
+    float valorDiv;
+
+    switch (dato){
+        case 1: valor = suma(Convert.ToInt32(num1), Convert.ToInt32(num2));
+        Console.WriteLine("El resultado es: " + valor);
+        break;
+
+        case 2: valor = resta(Convert.ToInt32(num1), Convert.ToInt32(num2));
+        Console.WriteLine("El resultado es: " + valor);
+        break;
+
+        case 3: valor = multiplicacion(Convert.ToInt32(num1), Convert.ToInt32(num2));
+        Console.WriteLine("El resultado es: " + valor);
+        break;
+
+        case 4: valorDiv = division(Convert.ToInt32(num1), Convert.ToInt32(num2));
+        Console.WriteLine("El resultado es: " + valorDiv);
+        break;
+
+    }
+
+    Console.WriteLine("\nDesea realizar otra operación?\nOPCIONES:\n[1]: Si\n[0]: No\n");
+    string otraOp = Console.ReadLine();
+    int decision = Convert.ToInt32(otraOp);
+
+    if (decision == 1){
+        dato = menu();
+        calculos(dato);
+    } else {
+        Console.WriteLine("Adiós");
+    }
+}
+
+int menu(){
+
+    Console.WriteLine("\nOPCIONES:\n[1]: Suma\n[2]: Resta\n[3]: Multiplicación\n[4]: División\nIngrese que operación desea realizar: ");
+    string opcion = Console.ReadLine();
+
+    return Convert.ToInt32(opcion);
+}
+
+
+int suma(int a, int b){
+
+    return a+b;
+}
+
+int resta(int a, int b){
+
+    return a-b;
+}
+
+int multiplicacion(int a, int b){
+
+    return a*b;
+}
+
+float division(int a, int b){
+
+    return a/b;
+}
